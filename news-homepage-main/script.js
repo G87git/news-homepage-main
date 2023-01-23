bc8719d916ebeb42abc969140c134c4ec6ab19b0
G87git <zahalargent.g87@gmail.com>
2023-01-23 11:28:18 +0100

    Let edit README and deploy

---
+++ news-homepage-main/script.js
@@ -0,0 +1,8 @@
+function openNav(){
+    document.getElementById("div-content").style.visibility="visible";
+    document.body.style.backgroundColor= "rgb(0,0,0,0.4)";
+}
+function closeNav(){
+    document.getElementById("div-content").style.visibility="hidden";
+    document.body.style.backgroundColor=" hsl(36, 100%, 99%)"
+}
